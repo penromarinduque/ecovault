69d75292ce0331beec881d7eb32de946e6cb4046
JeSaludo <janerissaludo@gmail.com>
2024-09-27 20:30:25 +0800

    Added Landing Page and Footer Layout

--- resources/views/layouts/user/partials/footer.blade.php
+++ resources/views/layouts/user/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="absolute bottom-0 w-full items-center">
-    <div class="bg-blue-900 h-8 justify-center flex">
+    <div class="bg-blue-900 py-4 justify-center flex">
         <h1 class="text-white">Department of Environment and Natural Resources © 2024</h1>
     </div>
 </footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,18 @@
 @section('title', 'PENRO Archiving System')
 
 @section('content')
-    <h1>Welcome to the Home Page</h1>
-    <p>This is the content of the home page.</p>
+    <div class="grid grid-flow-col m-14 gap-5">
+
+        <div>
+            <div class="bg-red-700 w-[720px] h-[380px]  rounded-lg "></div>
+        </div>
+        
+        <div class="space-y-3 m-4 mt-20">
+            <h1 class="font-bold text-3xl w-10/12">Welcome to Document Security and Digital Archiving System</h1>
+            <p class="text-xl font-medium">PENRO-Boac Marinduque</p>
+            <p class="w-9/12">Efficient document management system provides tailored solutions, enhancing workflow seamlessly</p>
+            <button class="bg-blue-400 rounded-md text-white h-10 px-8 ">Get Started</button>
+        </div>
+    
+    </div>
 @endsection
