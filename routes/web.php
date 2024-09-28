77c97150bc7fb16c283d6aa2bd42e85dbb9a6c8d
Mr.Kedge <101467604+MrKedge@users.noreply.github.com>
2024-09-28 15:14:51 +0800

    added register page

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Auth\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/admin', [AdminController::class, 'Home']);
+Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
