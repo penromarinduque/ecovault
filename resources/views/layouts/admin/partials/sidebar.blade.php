1526c7576830cb01ec30d8961610ba90a16ed680
Jan Eris Saludo <janerissaludo@gmail.com>
2024-10-19 13:40:54 +0800

    added url reference to adminstrative

--- resources/views/layouts/admin/partials/sidebar.blade.php
+++ resources/views/layouts/admin/partials/sidebar.blade.php
@@ -1,27 +1,27 @@
 <aside class="w-64 bg-[#363557] h-full  fixed top-20  py-4">
 
-    <div class="flex justify-center  mt-8 "> 
+    <div class="flex justify-center  mt-8 ">
         <button class="bg-white px-4 py-2 rounded-md">Hide Menu</button>
     </div>
-       
+
     <ul class="mx-4 flex flex-col space-y-8 mt-8 text-white">
         <li class="flex items-center space-x-2">
-            <a href="#" class="flex  gap-2 items-center">
+            <a href="{{ route('admin.home.show') }}" class="flex  gap-2 items-center">
                 <img src="{{ asset('images/navigation/dashboard.png') }}" alt="" class="w-16">
                 <span class="pb-4">Home</span>
             </a>
         </li>
-       
+
 
         <li class="flex items-center space-x-2">
-            <a href="{{route('file-manager.show')}}" class="flex  gap-2 items-center">
+            <a href="{{ route('file-manager.show') }}" class="flex  gap-2 items-center">
                 <img src="{{ asset('images/navigation/file-manager.png') }}" alt="" class="w-16">
                 <span class="">File Manager</span>
             </a>
         </li>
 
         <li class="flex items-center space-x-2">
-            <a href="#" class="flex  gap-2 items-center">
+            <a href="{{ route('admin.administrative.show') }}" class="flex  gap-2 items-center">
                 <img src="{{ asset('images/navigation/reports.png') }}" alt="" class="w-16">
                 <span class="">Adminstrive Documents</span>
             </a>
@@ -33,7 +33,7 @@
                 <span class="">Archive Files</span>
             </a>
         </li>
-        
+
         <li class="flex items-center space-x-2">
             <a href="#" class="flex  gap-2 items-center">
                 <img src="{{ asset('images/navigation/backup-and-recovery.png') }}" alt="" class="w-16">
@@ -41,7 +41,7 @@
             </a>
         </li>
     </ul>
-    
+
 
 
 </aside>
