f5a331e15f70029cdbea3f16082e7dbb90254513
Mr.Kedge <101467604+MrKedge@users.noreply.github.com>
2024-11-02 21:10:02 +0800

    added employee seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,11 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Municipality;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,13 +18,33 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
+        $faker = Faker::create();
         $this->call(AdminSeeder::class);
-        // User::factory(10)->create();
+        $this->call(AdminSeeder::class);
+        User::factory(count: 10)->create();
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        //     'employee_id' => random_int(1000000000, 9999999999),
-        // ]);
+        User::factory()->create([
+            'name' => $faker->name,
+            'email' => $faker->unique()->safeEmail,
+            'isAdmin' => false,
+            'employee_id' => Str::random(6),
+            'email_verified_at' => Carbon::now(),
+            'otp' => random_int(100000, 999999),
+            'password' => Hash::make('password'),
+        ]);
+
+
+        $locations = [
+            'Mogpog',
+            'Torrijos',
+            'Boac',
+            'Gasan',
+            'Buenavista',
+            'Sta. Cruz',
+        ];
+
+        foreach ($locations as $location) {
+            Municipality::create(['location' => $location]);
+        }
     }
 }
