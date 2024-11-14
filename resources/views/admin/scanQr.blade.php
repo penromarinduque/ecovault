fc3647b72cb43411df3247035febb8ff1b4444e5
Jan Eris Saludo <janerissaludo@gmail.com>
2024-11-15 01:17:46 +0800

    added qr code

--- resources/views/admin/scanQr.blade.php
+++ resources/views/admin/scanQr.blade.php
@@ -3,5 +3,31 @@
 @section('title', 'PENRO Archiving System')
 
 @section('content')
-    <h1>HELLO</h1>
+    <script src="https://unpkg.com/html5-qrcode"></script>
+    <div class="bg-white mx-auto w-6/12 rounded-lg p-4">
+
+        <div id="qr-reader"></div>
+        <div id="qr-reader-results"></div>
+    </div>
+
+    <script>
+        var resultContainer = document.getElementById('qr-reader-results');
+        var lastResult, countResults = 0;
+
+        function onScanSuccess(decodedText, decodedResult) {
+            if (decodedText !== lastResult) {
+                ++countResults;
+                lastResult = decodedText;
+                // Handle on success condition with the decoded message.
+                console.log(`Scan result ${decodedText}`, decodedResult);
+            }
+        }
+
+        var html5QrcodeScanner = new Html5QrcodeScanner(
+            "qr-reader", {
+                fps: 10,
+                qrbox: 350
+            });
+        html5QrcodeScanner.render(onScanSuccess);
+    </script>
 @endsection
