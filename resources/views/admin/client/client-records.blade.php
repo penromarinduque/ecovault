03d4d2f3be49bfc8b78ec384b00481efa2527ac7
Mr.Kedge <101467604+MrKedge@users.noreply.github.com>
2024-12-09 01:05:00 +0800

    added client card ui

--- resources/views/admin/client/client-records.blade.php
+++ resources/views/admin/client/client-records.blade.php
@@ -3,12 +3,139 @@
 @section('title', 'PENRO Archiving System')
 
 @section('content')
-    <table id="clients-table">
-        <!-- Table Headers will be dynamically generated -->
-    </table>
 
+    <div class="grid grid-cols-3 gap-6">
+        <section class="col-span-1 space-y-6 bg-white p-6 border border-gray-200 rounded-lg shadow">
+
+            <form class="max-w-md mx-auto">
+                <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
+                <div class="relative">
+                    <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                        <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 20 20">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                        </svg>
+                    </div>
+                    <input type="search" id="default-search"
+                        class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+                        placeholder="Search client name" required />
+                    <button type="submit"
+                        class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">Search</button>
+                </div>
+            </form>
+
+
+            <form class="max-w-md mx-auto">
+
+                <select id="large"
+                    class="block w-full px-4 py-3 text-base text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option disabled selected hidden class="text-green-500">Select permit</option>
+                    <option value="US">Tree Cutting Permits</option>
+                    <option value="CA">Tree Plantation</option>
+                    <option value="FR">Tree Transport Pemrit</option>
+                    <option value="DE">Chainsaw Registration</option>
+                    <option value="DE">Land Title</option>
+                </select>
+            </form>
+
+            <form class="max-w-md mx-auto">
+                <div class="flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
+                    <input id="bordered-checkbox-1" type="checkbox" value="" name="bordered-checkbox"
+                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                    <label for="bordered-checkbox-1"
+                        class="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Archived</label>
+                </div>
+            </form>
+
+
+        </section>
+
+
+        <section class="col-span-2">
+            <div class="w-full bg-white border border-gray-200 rounded-lg shadow">
+                <div class="bg-white shadow-md rounded-lg p-4 hover:shadow-lg transition">
+                    <h3 class="text-lg font-semibold mb-2">Land Title 123.pdf</h3>
+                    <p class="text-sm text-gray-600 mb-2"><strong>Owner:</strong> John Doe</p>
+
+
+                    <!-- Modal toggle -->
+                    <button data-modal-target="static-modal" data-modal-toggle="static-modal"
+                        class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        type="button">
+                        Toggle modal
+                    </button>
+
+                    <!-- Main modal -->
+                    <div id="static-modal" data-modal-backdrop="static" tabindex="-1" aria-hidden="true"
+                        class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                        <div class="relative p-4 w-full max-w-2xl max-h-full">
+                            <!-- Modal content -->
+                            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                <!-- Modal header -->
+                                <div
+                                    class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                                        Static modal
+                                    </h3>
+                                    <button type="button"
+                                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                        data-modal-hide="static-modal">
+                                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                            fill="none" viewBox="0 0 14 14">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                        </svg>
+                                        <span class="sr-only">Close modal</span>
+                                    </button>
+                                </div>
+                                <!-- Modal body -->
+                                <div class="p-4 md:p-5 space-y-4">
+                                    <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                                        With less than a month to go before the European Union enacts new consumer privacy
+                                        laws for its citizens, companies around the world are updating their terms of
+                                        service agreements to comply.
+                                    </p>
+                                    <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                                        The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect
+                                        on May 25 and is meant to ensure a common set of data rights in the European Union.
+                                        It requires organizations to notify users as soon as possible of high-risk data
+                                        breaches that could personally affect them.
+                                    </p>
+                                </div>
+                                <!-- Modal footer -->
+                                <div
+                                    class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                                    <button data-modal-hide="static-modal" type="button"
+                                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I
+                                        accept</button>
+                                    <button data-modal-hide="static-modal" type="button"
+                                        class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Decline</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </section>
+    </div>
 
     <script>
+        function toggleDetails(button) {
+            const details = button.nextElementSibling;
+            if (details.classList.contains("hidden")) {
+                details.classList.remove("hidden");
+                button.textContent = "Hide Details";
+            } else {
+                details.classList.add("hidden");
+                button.textContent = "Show Details";
+            }
+        }
+    </script>
+
+    {{-- <script>
         //love what you are doing
 
         let dataTable;
@@ -258,5 +385,5 @@
                 createDropdown(file.id);
             });
         }
-    </script>
+    </script> --}}
 @endsection
